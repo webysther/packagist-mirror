7a8496d40c96c5eb813c9492e44ab46f4d17a56f
rahman <rahman.hi@gmail.com>
2016-01-28 22:13:34 +0800

    Progressbar for check process

--- check.php
+++ check.php
@@ -1,4 +1,10 @@
 <?php
+
+use ProgressBar\Manager as ProgressBarManager;
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 /**
  * ダウンロードに失敗したファイルを一覧で出す
  */
@@ -14,24 +20,37 @@ $cachedir = $config->cachedir;
 
 $packagejson = json_decode(file_get_contents($cachedir.'packages.json'));
 
-$i = $j = 0;
+
+$j = 0;
+$errors = array();
+$providerCounter = 1;
+$numberOfProviders = count( (array)$packagejson->{'provider-includes'} );
+
 foreach ($packagejson->{'provider-includes'} as $tpl => $provider) {
     $providerjson = str_replace('%hash%', $provider->sha256, $tpl);
     $packages = json_decode(file_get_contents($cachedir.$providerjson));
 
+    $progressBar = new ProgressBarManager(0, count( (array)$packages->providers ));
+    $progressBar->setFormat("      - Package: %current%/%max% [%bar%] %percent%%");
+    echo "   - Check Provider {$providerCounter}/{$numberOfProviders}:\n";
+
     foreach ($packages->providers as $tpl2 => $sha) {
         if (!file_exists($file = $cachedir . "p/$tpl2\$$sha->sha256.json")) {
-            ++$i;
-            echo "$tpl\t$tpl2 file not exists\n";
+            $errors[] = "   - $tpl\t$tpl2 file not exists\n";
         } elseif ($sha->sha256 !== hash_file('sha256', $file)) {
-            ++$i;
             unlink($file);
-            echo "$tpl\t$tpl2\tsha256 not match: {$sha->sha256}\n";
+            $errors[] = "   - $tpl\t$tpl2\tsha256 not match: {$sha->sha256}\n";
         } else {
             ++$j;
         }
+        $progressBar->advance();
     }
-}
-echo $i, ' / ', $i+$j,  PHP_EOL;
 
-exit($i);
+    ++$providerCounter;
+}
+
+if (count($errors)) {
+    echo "Errors: \n", implode('', $errors);
+}
+
+exit(1);
