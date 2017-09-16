a50309e00a893a5957a120b47eb359fb8291dcad
Webysther Nunes <webysther@gmail.com>
2017-09-16 01:38:41 -0300

    Initial version

---
+++ src/Mirror.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * https://github.com/Webysther/mirror.
+ *
+ * @license CC0-1.0
+ */
+
+namespace League\Mirror;
+
+/**
+ * Create a mirror.
+ *
+ * @author Webysther Nunes <webysther@gmail.com>
+ */
+class Mirror
+{
+    
+}
