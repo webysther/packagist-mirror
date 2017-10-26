0722d04ffaddbef19934c5b25ed90e9cbd800d24
Webysther Nunes <webysther@gmail.com>
2017-10-26 19:59:53 -0200

    Start of decoupled version

---
+++ src/Http.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Packagist Mirror.
+ *
+ * For the full license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Webs\Mirror;
+
+/**
+ * Middleware to http operations.
+ *
+ * @author Webysther Nunes <webysther@gmail.com>
+ */
+class GZip
+{
+}
