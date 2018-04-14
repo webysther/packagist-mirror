da02ddf3f022c65523ab70fb036398aae7c09063
Webysther Nunes <Webysther@users.noreply.github.com>
2018-04-14 19:33:49 -0300

    Update index.html.php
    Removed information about sync time.

--- resources/index.html.php
+++ resources/index.html.php
@@ -58,7 +58,6 @@ h3.cmd {
 </header>
 <?php date_default_timezone_set('America/Sao_Paulo'); ?>
 <p align="center">Atualizado em <?= date('d/m/y H\hi') ?> horário de Brasília</p>
-<p align="center">(sincronizado a cada 15 segundos)</p>
 
 <br>
 <div class="pure-u-1-1" style="margin: 1em;">
