012da1fb3d195f7ec92082f4e5a34aac8ed97845
Eagle <eaglewudi@gmail.com>
2018-09-04 20:42:55 +0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -9,7 +9,6 @@ if [ ! -f $LOCK_FILE ]; then
 	echo $$ > $LOCK_FILE
 	$PHP_BIN $MIRROR_PATH/bin/mirror create
 	rsync -avz --recursive --stats $MIRROR_PATH/ cnpkg@cnpkg-wx:$MIRROR_PATH
-	rm -rf $LOCK_FILE
 
 	$MIRROR_PATH/rehash \
 	    -from $MIRROR_PATH/public/ \
@@ -17,6 +16,8 @@ if [ ! -f $LOCK_FILE ]; then
 
 	cp -f $MIRROR_PATH/public/index.html $MIRROR_PROXY_PATH/public/index.html
 	rsync --exclude="public/hashed.log" -avz --recursive --stats $MIRROR_PROXY_PATH/ cnpkg@cnpkg-wx:$MIRROR_PROXY_PATH
+    
+    rm -rf $LOCK_FILE
 else
 	pid=`cat $LOCK_FILE`
 	echo "Running: $pid"
